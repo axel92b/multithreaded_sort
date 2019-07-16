df41b81796aa4660215c79ba2ed99c4de780fda3
axel92b <axel92b@gmail.com>
2019-07-16 23:55:41 +0300

    Moved all code to header file, now generic, default param is 8 threads

--- mergeSortMT.hpp
+++ mergeSortMT.hpp
@@ -2,9 +2,59 @@
 #include <thread>
 
 //C++11
-void merge(int a[], int sizeA, int b[], int sizeB, int c[]);
-void mergeSortAuxMT(int* arr, int* temp, int size, int cur_depth, int max_depth);
+template<class T>
+void merge(T a[], int sizeA, T b[], int sizeB, T c[]){
+  int ia = 0, ib = 0, ic = 0;
+  while (ia < sizeA && ib < sizeB){
+    if (a[ia] <= b[ib]){
+      c[ic] = a[ia];
+      ia++;
+    }
+    else{
+      c[ic] = b[ib];
+      ib++;
+    }
+    ic++;
+  }
+  while (ia < sizeA){
+    c[ic] = a[ia];
+    ic++;
+    ia++;
+  }
+  while (ib < sizeB){
+    c[ic] = b[ib];
+    ic++;
+    ib++;
+  }
+}
+
+template<class T>
+void mergeSortAuxMT(T* arr, T* temp, int size, int cur_depth, int max_depth){
+  if (size < 2) {
+    return;
+  }
+  int left = size/2;
+  if (cur_depth <= max_depth) {
+    std::thread t(mergeSortAuxMT<T>, arr, temp, left, cur_depth+1, max_depth);
+    std::thread t1(mergeSortAuxMT<T>, (arr + left), temp, (size - left), cur_depth+1, max_depth);
+    t.join();
+    t1.join();
+  } else {
+    mergeSortAuxMT(arr, temp, left, cur_depth, max_depth);
+    mergeSortAuxMT(arr + left, temp, (size - left), cur_depth, max_depth);
+  }
+  merge(arr,left,arr+left,size-left,temp);
+  memcpy(arr, temp,size*sizeof(T));
+}
+
+
 //2^max_depth = number of working threads.
 //If current depth <= max_depth for each recursive call will be created new thread.
 //For example if max_depth = 3, then there will be 8 threads working on sorting.
-void mergeSortMT(int arr[], int size, int max_depth);
+template<class T>
+void mergeSortMT(T arr[], int size, int max_depth = 3){
+  T *temp = new T[size];
+  int depth = 1;
+  mergeSortAuxMT(arr, temp, size, depth, max_depth);
+  delete[] temp;
+}
