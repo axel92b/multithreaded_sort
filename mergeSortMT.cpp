d3b8cf741bae2a6618bd9418dc32a7518f6cc510
axel92b <axel92b@gmail.com>
2019-07-16 18:45:54 +0300

    typo fix

--- mergeSortMT.cpp
+++ mergeSortMT.cpp
@@ -33,8 +33,8 @@ void mergeSortAuxMT(int* arr, int* temp, int size, int cur_depth, int max_depth)
   }
   int left = size/2;
   if (cur_depth <= max_depth) {
-    std::thread t(mergeSortAuxMT2, arr, temp, left, cur_depth+1, max_depth);
-    std::thread t1(mergeSortAuxMT2, (arr + left), temp, (size - left), cur_depth+1, max_depth);
+    std::thread t(mergeSortAuxMT, arr, temp, left, cur_depth+1, max_depth);
+    std::thread t1(mergeSortAuxMT, (arr + left), temp, (size - left), cur_depth+1, max_depth);
     t.join();
     t1.join();
   } else {
@@ -48,6 +48,6 @@ void mergeSortAuxMT(int* arr, int* temp, int size, int cur_depth, int max_depth)
 void mergeSortMT(int arr[], int size, int max_depth){
   int *temp = new int[size];
   int depth = 1;
-  mergeSortAuxMT2(arr, temp, size, depth, max_depth);
+  mergeSortAuxMT(arr, temp, size, depth, max_depth);
   delete[] temp;
 }
