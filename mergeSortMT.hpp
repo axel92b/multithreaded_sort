b7a2adda43b9e52981ce7e19745256726ce624fa
axel92b <37740619+axel92b@users.noreply.github.com>
2019-07-16 18:56:43 +0300

    fixed typo

--- mergeSortMT.hpp
+++ mergeSortMT.hpp
@@ -6,5 +6,5 @@ void merge(int a[], int sizeA, int b[], int sizeB, int c[]);
 void mergeSortAuxMT(int* arr, int* temp, int size, int cur_depth, int max_depth);
 //2^max_depth = number of working threads.
 //If current depth <= max_depth for each recursive call will be created new thread.
-//For example is max_depth = 3, then there will be 8 threads working on sorting.
+//For example if max_depth = 3, then there will be 8 threads working on sorting.
 void mergeSortMT(int arr[], int size, int max_depth);
